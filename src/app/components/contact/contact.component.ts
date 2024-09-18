558503127c9e59e25ecbfeccd3824ac092e8d8a7
sentobc13 <sentoberlanga@gmail.com>
2024-09-18 13:51:54 +0200

    formulario creado y funcionando

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+  contactForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
 
+  onSubmit() {
+    console.log('form', this.contactForm.value);
+    this.contactForm.reset();
+  }
 }
