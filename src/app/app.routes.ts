63d3815f3d69b7e9437d2bd9c95f1bafb645bd6f
sentobc13 <sentoberlanga@gmail.com>
2024-09-18 13:26:33 +0200

    routes y componente contact creados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
     {path: "", component:HomeComponent},
-    {path: "about", component:AboutComponent}
+    {path: "about", component:AboutComponent},
+    {path: "contact", component:ContactComponent}
 ];
